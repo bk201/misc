cd51c2db11deb2582bea01da0772ba2e0a460968
Kiefer Chang <kiefer.chang@suse.com>
2023-08-10 15:54:16 +0800

    add fio script

---
+++ benchmark/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+DEVICE=$1
+
+echo "Cleanup the page cache..."
+echo 3 > /proc/sys/vm/drop_caches
+sleep 10
+
+echo "Start to benchmark raw device: $DEVICE"
+echo "Start to 1M Sequential Write..."
+fio --name=1M_SEQ_WRITE --ioengine=libaio --ramp_time=60 --filename=$DEVICE --rw=write --bs=1M --direct=1 --iodepth=32 --output=1M_seq_write.out
+
+echo "Cleanup the page cache..."
+echo 3 > /proc/sys/vm/drop_caches
+sleep 10
+
+echo "Start to 1M Sequential Read..."
+fio --name=1M_SEQ_READ --ioengine=libaio --ramp_time=60 --filename=$DEVICE --rw=read --bs=1M --direct=1 --iodepth=32 --output=1M_seq_read.out
+
+echo "Cleanup the page cache..."
+echo 3 > /proc/sys/vm/drop_caches
+sleep 10
+
+echo "Start to 4k Random Read..."
+fio --name=4K_RAND_READ --ioengine=libaio --ramp_time=60 --thread --random_generator=lfsr --cpus_allowed_policy=split --filename=$DEVICE --norandommap --rw=randread --bs=4k --direct=1 --iodepth=32 --numjobs=4 --group_reporting --output=4K_rand_read.out
+
+echo "Cleanup the page cache..."
+echo 3 > /proc/sys/vm/drop_caches
+sleep 10
+
+echo "Start to 4k Random Write..."
+fio --name=4K_RAND_WRITE --ioengine=libaio --ramp_time=60 --thread --random_generator=lfsr --cpus_allowed_policy=split  --filename=$DEVICE --norandommap --rw=randwrite --bs=4k --direct=1 --iodepth=32 --numjobs=4 --group_reporting --output=4K_rand_write.out
